7323cf71e9fa1d551c664bad39593111845f5e5d
Ali Da <al4icrux@gmail.com>
2021-03-16 15:06:18 +0300

    11 feat: add basic page layout;

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React from "react";
 import "./App.css";
+import logo from "./img/logo.png";
+import illustration from "./img/illustration.jpg";
 
 const App = () => {
   return (
-    <div>
-      <div> One div</div>
-      <div className="App">
-        <p> Hello , world!!!</p>
-        <ul>
-          <li>CSS</li>
-          <li>HTML</li>
-          <li>JS</li>
-          <li>React</li>
-        </ul>
+    <div className="app-wrapper">
+      <headers className="header">
+        <img src={logo} />
+      </headers>
+      <nav className="nav">
+        <div>
+          <a href="#">ProFile</a>
+        </div>
+        <div>
+          <a href="#">Users</a>
+        </div>
+        <div>
+          <a href="#">Music</a>
+        </div>
+        <div>
+          <a href="#">News</a>
+        </div>
+        <div>
+          <a href="#">Setting</a>
+        </div>
+      </nav>
+      <div className="content">
+        <img src={illustration} />
+        <div>ava + desc</div>
+        <div>
+          My post
+          <div>
+            new post:
+            <div>post 1</div>
+            <div>post 2</div>
+            <div>post 3</div>
+          </div>
+        </div>
       </div>
+      <footer className="footer"> footer </footer>
     </div>
   );
 };
