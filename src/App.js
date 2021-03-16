10940dc3dcec9a4c7aaec4091780822d7f8a8714
Ali Da <al4icrux@gmail.com>
2021-03-16 12:12:05 +0300

    6 feat: add basic page layout;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div> One div</div>
+      <div className="App">
+        <p> Hello , world!!!</p>
+        <ul>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>JS</li>
+          <li>React</li>
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
