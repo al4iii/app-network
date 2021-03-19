9211eefa307773bf9043d3afd1979176c07f6ebd
Ali Da <al4icrux@gmail.com>
2021-03-19 21:03:01 +0300

    37 refactor: add object store at state.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import state, {
-  addMessage,
-  addPost,
-  chengeNewPostText,
-  subscribe,
-  updateNewMassageText,
-} from "./redux/state";
+import store from "./redux/state";
 import App from "./App";
 
 const rerenderEntireTree = (state) => {
@@ -17,24 +11,25 @@ const rerenderEntireTree = (state) => {
     <BrowserRouter>
       <React.StrictMode>
         <App
-          posts={state.profilePage.posts}
-          dialogs={state.messagePage.dialogs}
-          messages={state.messagePage.messages}
-          addPost={addPost}
-          addMessage={addMessage}
-          chengeNewPostText={chengeNewPostText}
-          newPostText={state.profilePage.newPostText}
-          updateNewMassageText={updateNewMassageText}
-          newMassageText={state.messagePage.newMassageText}
+          stare={store.state}
+          posts={store._state.profilePage.posts}
+          dialogs={store._state.messagePage.dialogs}
+          messages={store._state.messagePage.messages}
+          addPost={store.addPost.bind(store)}
+          addMessage={store.addMessage.bind(store)}
+          chengeNewPostText={store.chengeNewPostText.bind(store)}
+          newPostText={store._state.profilePage.newPostText}
+          updateNewMassageText={store.updateNewMassageText.bind(store)}
+          newMassageText={store._state.messagePage.newMassageText}
         />
       </React.StrictMode>
     </BrowserRouter>,
     document.getElementById("root")
   );
 };
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,57 +1,65 @@
-let rerenderEntireTree = () => {};
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", like: 1 },
-      { id: 2, message: "Hi!!", like: 55 },
-      { id: 3, message: "I'm sexy!!", like: 144 },
-      { id: 4, message: "Have a good day", like: 15 },
-      { id: 5, message: "It's my first post!!", like: 177 },
-    ],
-    newPostText: "",
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", like: 1 },
+        { id: 2, message: "Hi!!", like: 55 },
+        { id: 3, message: "I'm sexy!!", like: 144 },
+        { id: 4, message: "Have a good day", like: 15 },
+        { id: 5, message: "It's my first post!!", like: 177 },
+      ],
+      newPostText: "",
+    },
+    messagePage: {
+      dialogs: [
+        { id: 1, name: "Dima", active: "active" },
+        { id: 2, name: "Alina", active: " " },
+        { id: 3, name: "Ninja", active: " " },
+        { id: 4, name: "Spider-man", active: " " },
+      ],
+      messages: [
+        { id: 1, message: "hello" },
+        { id: 2, message: "How are you" },
+        { id: 3, message: "yoyo" },
+        { id: 4, message: "hello-man " },
+      ],
+      newMassageText: "",
+    },
   },
-  messagePage: {
-    dialogs: [
-      { id: 1, name: "Dima", active: "active" },
-      { id: 2, name: "Alina", active: " " },
-      { id: 3, name: "Ninja", active: " " },
-      { id: 4, name: "Spider-man", active: " " },
-    ],
-    messages: [
-      { id: 1, message: "hello" },
-      { id: 2, message: "How are you" },
-      { id: 3, message: "yoyo" },
-      { id: 4, message: "hello-man " },
-    ],
-    newMassageText: "",
+  getState() {
+    return this._state;
+  },
+  _callsubscriber() {},
+  addPost() {
+    let post = this._state.profilePage.newPostText;
+    let newId = this._state.profilePage.posts.length + 2;
+    let newPost = { id: newId, message: post, like: 0 };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callsubscriber(this._state);
+  },
+  addMessage() {
+    debugger;
+    let message = this._state.messagePage.newMassageText;
+    let newId = this._state.messagePage.messages.length + 2;
+    let newMessage = { id: newId, message: message };
+    this._state.messagePage.messages.push(newMessage);
+    this._state.messagePage.newMassageText = "";
+    this._callsubscriber(this._state);
+  },
+  chengeNewPostText(post) {
+    debugger;
+    this._state.profilePage.newPostText = post;
+    this._callsubscriber(this._state);
+  },
+  updateNewMassageText(messege) {
+    this._state.messagePage.newMassageText = messege;
+    this._callsubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callsubscriber = observer;
   },
 };
 
-export const addPost = () => {
-  let post = state.profilePage.newPostText;
-  let newId = state.profilePage.posts.length + 2;
-  let newPost = { id: newId, message: post, like: 0 };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-export const addMessage = () => {
-  let message = state.messagePage.newMassageText;
-  let newId = state.messagePage.messages.length + 2;
-  let newMessage = { id: newId, message: message };
-  state.messagePage.messages.push(newMessage);
-  state.messagePage.newMassageText = "";
-  rerenderEntireTree(state);
-};
-export const chengeNewPostText = (post) => {
-  state.profilePage.newPostText = post;
-  rerenderEntireTree(state);
-};
-export const updateNewMassageText = (messege) => {
-  state.messagePage.newMassageText = messege;
-  rerenderEntireTree(state);
-};
-export const subscribe = (observer) => (rerenderEntireTree = observer);
-window.state = state;
-export default state;
+window.store = store;
+export default store;
