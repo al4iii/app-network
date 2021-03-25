6211121fb0f6966425db46a3eee7092da300e973
al4iii <al4icrux@gmail.com>
2021-03-25 11:11:50 +0300

    53 refactor: replaced the functional component Users with class;

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,60 +3,60 @@ import React from "react";
 import style from "./Users.module.css";
 import avatar from "./../../img/user-male.png";
 
-const Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          props.setUsers(response.data.items);
-        });
-    }
-  };
-  return (
-    <div>
-      <button onClick={getUsers}>add Users</button>
-      <div className={style.users}>
-        {props.users.map((u) => (
-          <div key={u.id} className={style.user}>
-            <span>
-              <div>
-                <img
-                  src={u.photos.small || avatar}
-                  alt="foto"
-                  className={style.userPhoto}
-                />
-              </div>
-              <div className={style.button}>
-                {u.followed ? (
-                  <button
-                    onClick={() => {
-                      props.unfollow(u.id);
-                    }}
-                  >
-                    follow
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      props.follow(u.id);
-                    }}
-                  >
-                    unfollow
-                  </button>
-                )}
-              </div>
-            </span>
-            <span className={style.user_info}>
-              <div className={style.item_info}>Name: {u.name}</div>
-              <div className={style.item_info}>{u.status || "no status"}</div>
-              <div className={style.item_info}> id : {u.id}</div>
-            </span>
-          </div>
-        ))}
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <div className={style.users}>
+          {this.props.users.map((u) => (
+            <div key={u.id} className={style.user}>
+              <span>
+                <div>
+                  <img
+                    src={u.photos.small || avatar}
+                    alt="foto"
+                    className={style.userPhoto}
+                  />
+                </div>
+                <div className={style.button}>
+                  {u.followed ? (
+                    <button
+                      onClick={() => {
+                        this.props.unfollow(u.id);
+                      }}
+                    >
+                      follow
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        this.props.follow(u.id);
+                      }}
+                    >
+                      unfollow
+                    </button>
+                  )}
+                </div>
+              </span>
+              <span className={style.user_info}>
+                <div className={style.item_info}>Name: {u.name}</div>
+                <div className={style.item_info}>{u.status || "no status"}</div>
+                <div className={style.item_info}> id : {u.id}</div>
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Users;
