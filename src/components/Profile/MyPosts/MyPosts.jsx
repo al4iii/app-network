eecb47bab153ac31e7149bf3cd0528e8754f36e0
al4iii <al4icrux@gmail.com>
2021-04-07 10:45:41 +0300

    88 refactor: pure function (.reverve());

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,11 +3,15 @@ import Post from "./Post/Post";
 import styles from "./MyPosts.module.css";
 
 const MyPosts = React.memo((props) => {
-  let postElemenst = props.posts.map((p) => (
-    <Post messege={p.message} like={p.like}
-      key={p.id} myProfilePhoto={props.myProfilePhoto}
-    />
-  ));
+  let postElemenst = [...props.posts]
+    .reverse().map((p) => (
+      <Post
+        messege={p.message}
+        like={p.like}
+        key={p.id}
+        myProfilePhoto={props.myProfilePhoto}
+      />
+    ));
   return (
     <div className={styles.post}>
       <h3 className={styles.h3}>My posts:</h3>
