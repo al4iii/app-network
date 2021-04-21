2d5bb6085f181e9497aaca4370b1cb5533ac6975
al4iii <al4icrux@gmail.com>
2021-04-21 14:06:52 +0300

    108.1 feat: add music component;

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import { useState, useRef } from "react";
+import song from "./ACDC Recovered - Highway to Hell.mp3";
+import ControlPanel from "./components/controls/ControlPanel";
+import Slider from "./components/slider/Slider";
+
 import styles from "./Music.module.css";
 
-const Music = (props) => {
+const Music = () => {
+  const [percentage, setPercentage] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const audioRef = useRef();
+  const onChange = (e) => {
+    const audio = audioRef.current;
+    audio.currentTime = (audio.duration / 100) * e.target.value;
+    setPercentage(e.target.value);
+  };
+  const play = () => {
+    const audio = audioRef.current;
+    audio.volume = 0.1;
+    if (!isPlaying) {
+      setIsPlaying(true);
+      audio.play();
+    }
+    if (isPlaying) {
+      setIsPlaying(false);
+      audio.pause();
+    }
+  };
+  const getCurrDuration = (e) => {
+    const percent = (
+    (e.currentTarget.currentTime / e.currentTarget.duration) * 100).toFixed(2);
+    const time = e.currentTarget.currentTime;
+    setPercentage(+percent);
+    setCurrentTime(time.toFixed(2));
+  };
   return (
-    <div className={styles.music}>
-      <h4> Music</h4>
-      <p>This section is under construction.</p>
+    <div className="app-container">
+      <h1 className={styles.h1}>Audio Player</h1>
+      <Slider percentage={percentage} onChange={onChange} />
+      <audio
+        ref={audioRef}
+        onTimeUpdate={getCurrDuration}
+        onLoadedData={(e) => {
+          setDuration(e.currentTarget.duration.toFixed(2));
+        }}
+        src={song}
+      ></audio>
+      <ControlPanel
+        play={play}
+        isPlaying={isPlaying}
+        duration={duration}
+        currentTime={currentTime}
+      />
     </div>
   );
 };
