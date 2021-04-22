6aaa48263a908dfa548b9ce9a183c41dbd871d18
al4iii <al4icrux@gmail.com>
2021-04-22 17:33:34 +0300

    Updates

---
+++ static/js/5.8548a91b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[5],{302:function(n,s,c){},311:function(n,s,c){"use strict";c.r(s);c(1),c(302);var t=c(0);s.default=function(n){return Object(t.jsxs)("div",{children:[Object(t.jsx)("h4",{children:" News"}),Object(t.jsx)("p",{children:"This section is under construction."})]})}}}]);
+//# sourceMappingURL=5.8548a91b.chunk.js.map
