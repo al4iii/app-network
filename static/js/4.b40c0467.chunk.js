cab0be5954ebdcfdbfa82f74da60021eca7f97bb
al4iii <al4icrux@gmail.com>
2021-05-05 17:15:29 +0300

    Updates

---
+++ static/js/4.b40c0467.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[4],{397:function(s,c,e){"use strict";e.r(c);e(0);var n=e(1);c.default=function(){return Object(n.jsxs)("div",{children:[Object(n.jsx)("h4",{children:" News"}),Object(n.jsx)("p",{children:"This section is under construction."})]})}}}]);
+//# sourceMappingURL=4.b40c0467.chunk.js.map
