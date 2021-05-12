8907c6d208ceaed57654c593e83dccc598d5b800
al4iii <al4icrux@gmail.com>
2021-05-12 23:24:22 +0300

    Updates

---
+++ static/js/4.2e31367d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[4],{621:function(s,c,e){"use strict";e.r(c);e(0);var n=e(1);c.default=function(){return Object(n.jsxs)("div",{children:[Object(n.jsx)("h4",{children:" News"}),Object(n.jsx)("p",{children:"This section is under construction."})]})}}}]);
+//# sourceMappingURL=4.2e31367d.chunk.js.map
--- static/js/4.bd892d9f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[4],{598:function(s,c,e){"use strict";e.r(c);e(0);var n=e(1);c.default=function(){return Object(n.jsxs)("div",{children:[Object(n.jsx)("h4",{children:" News"}),Object(n.jsx)("p",{children:"This section is under construction."})]})}}}]);
-//# sourceMappingURL=4.bd892d9f.chunk.js.map
